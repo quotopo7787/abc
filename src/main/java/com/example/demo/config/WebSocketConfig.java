5192185b8e09b83006e0a8f2105605afc735e319
VNEXT\QuangTN <quangtn@vnext.com.vn>
2022-10-17 09:32:24 +0700

    no message

---
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import com.example.demo.interceptor.HttpHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
+  	@Bean
+  	public ClassLoaderTemplateResolver emailTemplateResolver() {
+  		ClassLoaderTemplateResolver pdfTemplateResolver = new ClassLoaderTemplateResolver();
+  		pdfTemplateResolver.setPrefix("static/templates/");
+  		pdfTemplateResolver.setSuffix(".html");
+  		pdfTemplateResolver.setTemplateMode("HTML5");
+  		pdfTemplateResolver.setCharacterEncoding("UTF-8");
+  		pdfTemplateResolver.setOrder(1);
+  		return pdfTemplateResolver;
+  	}
+
+}
